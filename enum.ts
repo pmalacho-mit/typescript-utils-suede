369f9dce79aca508d6eec9370fff740a23eff8d9
GitHub Action <action@github.com>
2026-03-27 00:40:41 +0000

    Revert "adding styled components (likely to move them to their own repo)"
    This reverts commit 35f4ab5eec3de86550fd131001b18b6582d8dd5b.

--- enum.ts
+++ enum.ts
@@ -1,29 +1,11 @@
-export function enumify<const T extends readonly string[]>(
-  ...values: T
-): { [K in T[number]]: K } & { _values: T };
-export function enumify<const T extends Record<string, unknown>>(
-  values: T,
-): { [K in Extract<keyof T, string>]: K } & {
-  _values: readonly Extract<keyof T, string>[];
-};
-export function enumify(
-  ...args: readonly [Record<string, unknown>] | readonly string[]
-) {
-  const values =
-    args.length === 1 && typeof args[0] === "object" && args[0] !== null
-      ? Object.keys(args[0])
-      : (args as readonly string[]);
-
-  return values.reduce(
+export const enumify = <const T extends string[]>(...values: T) =>
+  values.reduce(
     (acc, value) => {
       (acc as any)[value] = value;
       return acc;
     },
-    { _values: values } as Record<string, string> & {
-      _values: readonly string[];
-    },
+    { _values: values } as { [k in T[number]]: k } & { _values: T },
   );
-}
 
-export type Enumified<T extends { _values: readonly string[] }> =
+export type Enumified<T extends ReturnType<typeof enumify<any>>> =
   T["_values"][number];
--- regex.ts
+++ regex.ts
@@ -1,4 +1,1 @@
 export const hasNonWhiteSpace = (str: string) => /\S/.test(str);
-
-export const word = (word: string, global = true) =>
-  new RegExp(`\\b${word}\\b`, global ? "g" : "");
